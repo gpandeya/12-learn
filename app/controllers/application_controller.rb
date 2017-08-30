836c8d949b4124df80852727052186eaebb5aa18
Gyanesh Pandeya <gyanesh.pandeya@gmail.com>
2017-08-30 17:36:49 +0000

    looger

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :get_user
+  before_action :get_user, :log_auth
+  
 
   private
+  
+  def log_auth
+    data ={uid: nil, email: nil, role: nil, controller: controller_name, action: action_name, params: request.params.to_h, time: Time.now}
+    data= data.merge ({uid: @user.id, email: @user.email, role: @user.role}) if @user
+    #final =data.merge(user)
+    ap data
+  end
+  
 
   def get_user
     @user = current_user
@@ -18,4 +27,5 @@ class ApplicationController < ActionController::Base
   def auth_student
     redirect_to root_url if !@is_student
   end
+
 end
